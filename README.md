e73410dccface0c767a2a2710ae669fff3a91941
Ken Snyder <kendsnyder@gmail.com>
2015-03-11 19:38:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+= Hands On with lodash =
+
+A walkthrough on lodash at the Learners JS meetup March 17, 2015 by Ken Snyder
+
+[View Slides](http://intel.to/lodash-slides)
+
---
+++ js/buildings.js
@@ -0,0 +1,401 @@
+var buildings = [{
+    "rank": 1,
+    "name": "Burj Khalifa",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 2717,
+    "floors": 163,
+    "year": 2010
+}, {
+    "rank": 2,
+    "name": "Shanghai Tower",
+    "city": "Shanghai",
+    "country": "China",
+    "height": 2073,
+    "floors": 121,
+    "year": 2015
+}, {
+    "rank": 3,
+    "name": "Makkah Royal Clock Tower Hotel",
+    "city": "Mecca",
+    "country": "Saudi Arabia",
+    "height": 1971,
+    "floors": 120,
+    "year": 2012
+}, {
+    "rank": 4,
+    "name": "One World Trade Center",
+    "city": "New York City",
+    "country": "USA",
+    "height": 1776,
+    "floors": 104,
+    "year": 2014
+}, {
+    "rank": 5,
+    "name": "CTF Finance Centre",
+    "city": "Guangzhou",
+    "country": "China",
+    "height": 1740,
+    "floors": 111,
+    "year": 2016
+}, {
+    "rank": 6,
+    "name": "Taipei 101",
+    "city": "Taipei",
+    "country": "Taiwan",
+    "height": 1670,
+    "floors": 101,
+    "year": 2004
+}, {
+    "rank": 7,
+    "name": "Shanghai World Financial Center",
+    "city": "Shanghai",
+    "country": "China",
+    "height": 1614,
+    "floors": 101,
+    "year": 2008
+}, {
+    "rank": 8,
+    "name": "International Commerce Centre",
+    "city": "Hong Kong",
+    "country": "Hong Kong",
+    "height": 1588,
+    "floors": 118,
+    "year": 2010
+}, {
+    "rank": 9,
+    "name": "Petronas Tower 1",
+    "city": "Kuala Lumpur",
+    "country": "Malaysia",
+    "height": 1483,
+    "floors": 88,
+    "year": 1998
+}, {
+    "rank": 10,
+    "name": "Petronas Tower 2",
+    "city": "Kuala Lumpur",
+    "country": "Malaysia",
+    "height": 1483,
+    "floors": 88,
+    "year": 1998
+}, {
+    "rank": 11,
+    "name": "Zifeng Tower",
+    "city": "Nanjing",
+    "country": "China",
+    "height": 1476,
+    "floors": 89,
+    "year": 2010
+}, {
+    "rank": 12,
+    "name": "Willis Tower (Formerly Sears Tower)",
+    "city": "Chicago",
+    "country": "USA",
+    "height": 1450,
+    "floors": 108,
+    "year": 1973
+}, {
+    "rank": 13,
+    "name": "KK100",
+    "city": "Shenzhen",
+    "country": "China",
+    "height": 1449,
+    "floors": 100,
+    "year": 2011
+}, {
+    "rank": 14,
+    "name": "Guangzhou International Finance Center",
+    "city": "Guangzhou",
+    "country": "China",
+    "height": 1440,
+    "floors": 103,
+    "year": 2010
+}, {
+    "rank": 15,
+    "name": "432 Park Avenue",
+    "city": "New York City",
+    "country": "USA",
+    "height": 1397,
+    "floors": 96,
+    "year": 2015
+}, {
+    "rank": 16,
+    "name": "Trump International Hotel and Tower",
+    "city": "Chicago",
+    "country": "USA",
+    "height": 1389,
+    "floors": 98,
+    "year": 2009
+}, {
+    "rank": 17,
+    "name": "Jin Mao Tower",
+    "city": "Shanghai",
+    "country": "China",
+    "height": 1380,
+    "floors": 88,
+    "year": 1999
+}, {
+    "rank": 18,
+    "name": "Princess Tower",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1358,
+    "floors": 101,
+    "year": 2012
+}, {
+    "rank": 19,
+    "name": "Al Hamra Tower",
+    "city": "Kuwait City",
+    "country": "Kuwait",
+    "height": 1354,
+    "floors": 80,
+    "year": 2011
+}, {
+    "rank": 20,
+    "name": "2 International Finance Centre",
+    "city": "Hong Kong",
+    "country": "Hong Kong",
+    "height": 1352,
+    "floors": 88,
+    "year": 2003
+}, {
+    "rank": 21,
+    "name": "23 Marina",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1296,
+    "floors": 89,
+    "year": 2012
+}, {
+    "rank": 22,
+    "name": "CITIC Plaza",
+    "city": "Guangzhou",
+    "country": "China",
+    "height": 1283,
+    "floors": 80,
+    "year": 1997
+}, {
+    "rank": 23,
+    "name": "Capital Market Authority Headquarters",
+    "city": "Riyadh",
+    "country": "Saudi Arabia",
+    "height": 1263,
+    "floors": 85,
+    "year": 2014
+}, {
+    "rank": 24,
+    "name": "Shun Hing Square",
+    "city": "Shenzhen",
+    "country": "China",
+    "height": 1260,
+    "floors": 69,
+    "year": 1996
+}, {
+    "rank": 25,
+    "name": "Burj Mohammed bin Rashid",
+    "city": "Abu Dhabi",
+    "country": "UAE",
+    "height": 1251,
+    "floors": 88,
+    "year": 2014
+}, {
+    "rank": 26,
+    "name": "Empire State Building",
+    "city": "New York City",
+    "country": "USA",
+    "height": 1250,
+    "floors": 102,
+    "year": 1931
+}, {
+    "rank": 27,
+    "name": "Elite Residence",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1247,
+    "floors": 87,
+    "year": 2012
+}, {
+    "rank": 28,
+    "name": "Tuntex Sky Tower",
+    "city": "Kaohsiung",
+    "country": "Taiwan",
+    "height": 1240,
+    "floors": 85,
+    "year": 1997
+}, {
+    "rank": 29,
+    "name": "Central Plaza",
+    "city": "Hong Kong",
+    "country": "Hong Kong",
+    "height": 1227,
+    "floors": 78,
+    "year": 1992
+}, {
+    "rank": 30,
+    "name": "Bank of China Tower",
+    "city": "Hong Kong",
+    "country": "Hong Kong",
+    "height": 1205,
+    "floors": 70,
+    "year": 1990
+}, {
+    "rank": 31,
+    "name": "Bank of America Tower",
+    "city": "New York City",
+    "country": "USA",
+    "height": 1200,
+    "floors": 54,
+    "year": 2009
+}, {
+    "rank": 32,
+    "name": "Almas Tower",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1191,
+    "floors": 68,
+    "year": 2009
+}, {
+    "rank": 33,
+    "name": "The Pinnacle",
+    "city": "Guangzhou",
+    "country": "China",
+    "height": 1181,
+    "floors": 60,
+    "year": 2012
+}, {
+    "rank": 34,
+    "name": "SEG Plaza",
+    "city": "Shenzhen",
+    "country": "China",
+    "height": 1168,
+    "floors": 70,
+    "year": 2000
+}, {
+    "rank": 35,
+    "name": "JW Marriott Marquis Dubai Tower 1",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1166,
+    "floors": 82,
+    "year": 2012
+}, {
+    "rank": 36,
+    "name": "JW Marriott Marquis Dubai Tower 2",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1166,
+    "floors": 82,
+    "year": 2013
+}, {
+    "rank": 37,
+    "name": "Emirates Office Tower",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1163,
+    "floors": 54,
+    "year": 2000
+}, {
+    "rank": 38,
+    "name": "Aon Center",
+    "city": "Chicago",
+    "country": "USA",
+    "height": 1136,
+    "floors": 83,
+    "year": 1973
+}, {
+    "rank": 39,
+    "name": "The Center",
+    "city": "Hong Kong",
+    "country": "Hong Kong",
+    "height": 1135,
+    "floors": 73,
+    "year": 1998
+}, {
+    "rank": 40,
+    "name": "Keangnam Hanoi Landmark Tower",
+    "city": "Hanoi",
+    "country": "Vietnam",
+    "height": 1132,
+    "floors": 72,
+    "year": 2011
+}, {
+    "rank": 41,
+    "name": "John Hancock Center",
+    "city": "Chicago",
+    "country": "USA",
+    "height": 1128,
+    "floors": 100,
+    "year": 1969
+}, {
+    "rank": 42,
+    "name": "Ahmed Abdul Rahim Al Attar Tower",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1122,
+    "floors": 76,
+    "year": 2010
+}, {
+    "rank": 43,
+    "name": "Chongqing World Financial Center",
+    "city": "Chongqing",
+    "country": "China",
+    "height": 1112,
+    "floors": 78,
+    "year": 2014
+}, {
+    "rank": 44,
+    "name": "Hefei Feicui TV Tower",
+    "city": "Hefei",
+    "country": "China",
+    "height": 1112,
+    "floors": 75,
+    "year": 2012
+}, {
+    "rank": 45,
+    "name": "Mercury City Tower",
+    "city": "Moscow",
+    "country": "Russia",
+    "height": 1112,
+    "floors": 75,
+    "year": 2012
+}, {
+    "rank": 46,
+    "name": "The Wharf Times Square",
+    "city": "Wuxi",
+    "country": "China",
+    "height": 1112,
+    "floors": 68,
+    "year": 2014
+}, {
+    "rank": 47,
+    "name": "Tianjin World Financial Center",
+    "city": "Tianjin",
+    "country": "China",
+    "height": 1105,
+    "floors": 79,
+    "year": 2010
+}, {
+    "rank": 48,
+    "name": "The Marina Torch",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 1105,
+    "floors": 79,
+    "year": 2011
+}, {
+    "rank": 49,
+    "name": "Shimao International Plaza",
+    "city": "Shanghai",
+    "country": "China",
+    "height": 194,
+    "floors": 61,
+    "year": 2005
+}, {
+    "rank": 50,
+    "name": "Rose Tower",
+    "city": "Dubai",
+    "country": "UAE",
+    "height": 193,
+    "floors": 72,
+    "year": 2007
+}];
